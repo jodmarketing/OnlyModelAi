7c54486eb3c77df209fea4701e02e393dc949353
jodmarketing <moecompany528@gmail.com>
2025-07-22 10:37:28 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", () => {
-  const hero = document.querySelector(".hero");
-  hero.style.opacity = 0;
-  setTimeout(() => {
-    hero.style.transition = "opacity 1.5s ease-in-out";
-    hero.style.opacity = 1;
-  }, 500);
-});
